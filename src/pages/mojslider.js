8839a07726c8d6a488fd29c7022ee7cc37b8f604
Zoran Dasic <zorandsc1@gmail.com>
2021-01-08 11:43:31 +0100

    sve stavljeno u mojslider baseline

--- src/pages/mojslider.js
+++ src/pages/mojslider.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components"
 //import { useSpring, animated } from 'react-spring'
 
@@ -7,29 +7,54 @@ import Slide from "../components/Slide"
 import slides from "../components/slideData"
 
 const Mojslider = () => {
-    const [slideIndex, setSlideIndex]=useState(0)
+    const [slideIndex, setSlideIndex] = useState(0)
 
-    
-    const handleNext=()=>{
-        setSlideIndex((slideIndex +1)%slides.length)
+
+    const handleNext = () => {
+        setSlideIndex((slideIndex + 1) % slides.length)
     }
-    const handlePrev=()=>{
-        
-        slideIndex===0 ? 
-            setSlideIndex(slides.length-1) : 
-            setSlideIndex(slideIndex-1)
-        
+    const handlePrev = () => {
+
+        slideIndex === 0 ?
+            setSlideIndex(slides.length - 1) :
+            setSlideIndex(slideIndex - 1)
+
     }
 
     return (
         <Layout>
             <Wrapper>
                 <div className="slides">
-                    
 
-                    {[...slides,...slides,...slides].map((slide, i)=>{
-                        let offset=slides.length+(slideIndex-i)
-                        return <Slide slide={slide} offset={offset} key={i}></Slide>
+
+                    {[...slides, ...slides, ...slides].map((slide, i) => {
+                        let offset = slides.length + (slideIndex - i)
+                        const active = offset === 0 ? true : null;
+                        let dir = offset === 0 ? 0 : offset > 0 ? 1 : -1
+                        return (
+                            <div className="slide" >
+                                <div className="slideBackground"
+                                    style={{
+                                        backgroundImage: `url('${slide.image}')`,
+                                        transform: `translateX(calc(10% * ${dir}))`
+                                    }}>
+                                </div>
+                                <div className="slideContent"
+                                    style={{
+                                        backgroundImage: `url('${slide.image}')`,
+                                        transform: `perspective(1000px) 
+                                translateX(calc(100% * ${offset}))
+                                rotateY(calc(-45deg * ${dir}))
+                                `
+                                    }}>
+                                    <div className="slideContentInner">
+                                        <h2 className="slideTitle">{slide.title}</h2>
+                                        <h3 className="slideSubtitle">{slide.subtitle}</h3>
+                                        <p className="slideDescription">{slide.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
                     })}
                     <button className="prev" onClick={handlePrev}>PREV</button>
                     <button className="next" onClick={handleNext}>NEXT</button>
@@ -49,8 +74,30 @@ const Wrapper = styled.div`
         height:100vh;
         display:grid;
         justify-content:center;
-        align-items:center;
-        
+        align-items:center;   
+    }
+    .slide{
+        grid-area:1/-1;
+    }
+
+    .slideContent {
+        width:30vw;
+        height:40vw;
+        background-size:cover;
+        background-position:center center;
+        background-repeat:no-repeat;
+        transform-style:preserve-3d;
+    }
+    .slideBackground{
+        position:fixed;
+        top:0;
+        left: -10%;
+        right: -10%;
+        bottom: 0;
+        background-size: cover;
+        background-position: center center;
+        z-index: -1;
+        opacity:0;
     }
 
     button{
